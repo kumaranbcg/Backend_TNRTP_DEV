32e393ef90915414b624e6b7af42552ffa2caf35
kumaranbcg <kumaran@doodleblue.com>
2020-08-12 10:08:29 +0530

    Update pcFormBankDetails.model.js

--- application/models/application/pcFormBankDetails.model.js
+++ application/models/application/pcFormBankDetails.model.js
@@ -1,4 +1,6 @@
 const { DELETE_STATUS, FORM_SECTION_STATUS } = require("../../constants/index");
+const Cryptr = require("cryptr");
+const cryptr = new Cryptr(process.env.AES_KEY);
 module.exports = (sequelize, DataTypes) => {
 	const pcFormBankDetails = sequelize.define(
 		"TNRTP11_PC_FORMS_BANK_DETAILS",
