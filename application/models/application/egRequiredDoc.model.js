637886b2ed28590b2f2b01564f3224e5ebb2c8b9
Ashik <mohamed.ashik@doodleblue.com>
2020-08-18 18:25:08 +0530

    staff api development

--- application/models/application/egRequiredDoc.model.js
+++ application/models/application/egRequiredDoc.model.js
@@ -1,22 +1,22 @@
 module.exports = (sequelize, DataTypes) => {
 	const egRequiredDoc = sequelize.define(
-		"TNRTP106_SELECTED_EG_REQUIRED_DOCUMENT",
+		"TNRTP109_SELECTED_EG_REQUIRED_DOCUMENT",
 		{
-			TNRTP106_SELECTED_EG_REQUIRED_DOCUMENT_D: {
+			TNRTP109_SELECTED_EG_REQUIRED_DOCUMENT_D: {
 				type: DataTypes.INTEGER,
 				primaryKey: true,
 				autoIncrement: true,
 			},
 			appStatusId: {
 				type: DataTypes.INTEGER,
-				field: "TNRTP106_EG_APPLICATION_STATUS_MASTER_D",
+				field: "TNRTP109_EG_APPLICATION_STATUS_MASTER_D",
 			},
-			docName: { type: DataTypes.STRING, field: "TNRTP106_EG_REQUIRED_DOCUMENT_NAME_N" },
-			docUrl: { type: DataTypes.STRING, field: "TNRTP106_EG_REQUIRED_DOCUMENT_URL_N" },
-			docType: { type: DataTypes.INTEGER, field: "TNRTP106_REQUIRED_DOCUMENT_TYPE_D" },
-			docStatus: { type: DataTypes.INTEGER, field: "TNRTP106_STATUS_D" },
-			TNRTP106_CREATED_AT: { type: DataTypes.DATE },
-			TNRTP106_UPDATED_AT: { type: DataTypes.DATE },
+			docName: { type: DataTypes.STRING, field: "TNRTP109_EG_REQUIRED_DOCUMENT_NAME_N" },
+			docUrl: { type: DataTypes.STRING, field: "TNRTP109_EG_REQUIRED_DOCUMENT_URL_N" },
+			docType: { type: DataTypes.INTEGER, field: "TNRTP109_REQUIRED_DOCUMENT_TYPE_D" },
+			docStatus: { type: DataTypes.INTEGER, field: "TNRTP109_STATUS_D" },
+			TNRTP109_CREATED_AT: { type: DataTypes.DATE },
+			TNRTP109_UPDATED_AT: { type: DataTypes.DATE },
 		},
 		{
 			freezeTableName: true,
